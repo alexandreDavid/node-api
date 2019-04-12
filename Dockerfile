8a0bc05e9c7cb01a742dd91536eda71fae92da28
Alexandre David <a.david@rheagroup.com>
2019-04-12 09:46:03 +0100

    Configuration from another repo

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:lts-alpine
+
+WORKDIR /usr/src/app
+
+ARG NODE_ENV
+ENV NODE_ENV $NODE_ENV
+
+COPY package.json /usr/src/app/
+RUN npm install
+
+COPY . /usr/src/app
+
+ENV PORT 5000
+EXPOSE $PORT
+CMD [ "npm", "start" ]
--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'dfms_users',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 module.exports = pool
