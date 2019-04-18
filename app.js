407502e5d98718b0b792279d0d2640235860091b
Alexandre David <a.david@rheagroup.com>
2019-04-18 15:35:25 +0100

    Env before db

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-
 require('dotenv').config();
+
+var index = require('./routes/index');
 
 var app = express();
 
